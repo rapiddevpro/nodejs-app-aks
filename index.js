fc50a9d28b696f31149655fd8b2e90487daa2e63
UjjawalSolanki <ujjawal.solanki>
2019-05-28 00:08:55 +0530

    minor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('*', (req, res, next) => {
-  res.send('<h1>Hello world</h1>');
+  res.send('<h1>Hello there! wassup</h1>');
 });
 
 app.listen(4000, () => {
